7015605e5916973498fad0b2b1d17d726f2b10a1
CalvinPVIII <calvinp003@gmail.com>
2024-05-15 19:31:37 -0700

    update responsiveness of resume

--- src/Components/ResumeContent.tsx
+++ src/Components/ResumeContent.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import "../styles/ResumeContent.css";
+import "../styles/ResumeContentSmall.css";
 import resume from "../ts/resume";
 import { motion } from "framer-motion";
+import { useMediaQuery } from "react-responsive";
 export default function ResumeContent() {
+  const isSmallScreen = useMediaQuery({ query: "(max-width: 1000px)" });
   return (
-    <div id="resume-content-wrapper">
-      <h1 className="resume-header reveal-text">work experience</h1>
+    <div id={isSmallScreen ? "resume-content-wrapper-small" : "resume-content-wrapper"}>
+      <h1 className={isSmallScreen ? "resume-header-small reveal-text" : "resume-header reveal-text"}>work experience</h1>
       <motion.div
         initial={{ scaleX: 0 }}
         animate={{ scaleX: 1 }}
         transition={{ duration: 0.4, delay: 0.6 }}
-        className="underline"
-        style={{ width: "265px", marginTop: "-24px", marginLeft: "320px" }}
+        className={isSmallScreen ? "underline small-underline" : "underline"}
+        style={isSmallScreen ? { width: "16rem", marginTop: "-24px" } : { width: "265px", marginTop: "-24px", marginLeft: "320px" }}
       ></motion.div>
+
       <div id="resume-list-wrapper">
         <motion.div initial={{ scaleY: 0 }} animate={{ scaleY: 1 }} transition={{ duration: 0.8 }} id="list-left-border"></motion.div>
 
@@ -30,13 +34,17 @@ export default function ResumeContent() {
               <p className="bullet-content reveal-text-right">{job.description}</p>
             </React.Fragment>
           ))}
-          <h1 className="underline-text resume-header reveal-text">education</h1>
+          <h1 className={isSmallScreen ? "resume-header-small reveal-text" : "resume-header reveal-text"}>education</h1>
           <motion.div
             initial={{ scaleX: 0 }}
             animate={{ scaleX: 1 }}
             transition={{ duration: 0.4, delay: 0.6 }}
-            className="underline"
-            style={{ width: "165px", marginTop: "-24px", marginLeft: "370px" }}
+            className={isSmallScreen ? "underline small-underline" : "underline"}
+            style={
+              isSmallScreen
+                ? { width: "10rem", marginTop: "-24px", marginBottom: "20px" }
+                : { width: "265px", marginTop: "-24px", marginLeft: "320px" }
+            }
           ></motion.div>
           {resume.education.map((job) => (
             <React.Fragment key={job.title}>
